3d691a4d97560af94c12cc87d8fbda449ac0ebc6
David Studholme <d.j.studholme@exeter.ac.uk>
2026-08-10 07:22:36 +0000

    Ran test on first set of primers

---
+++ 07_dipper2/genomes-all-rssc-complete-genomes/commands.sh
@@ -0,0 +1,9 @@
+### Download NCBI's datasets utility:
+curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'
+chmod u+x datasets
+
+### Use NCBI's datasets utility to download the genome sequences, unzip them and move to current directory:
+./datasets download genome accession --inputfile assembly_accessions.all.txt --include genome --filename genome_assemblies.zip
+unzip genome_assemblies.zip
+mv ncbi_dataset/data/GC*_*/GC*_*.fna .
+
