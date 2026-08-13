71148d5b5318d472499f0cda21efe83ffea69aee
David Studholme <d.j.studholme@exeter.ac.uk>
2026-08-13 10:48:44 +0000

    Script now surveys NCBI and PhytoBacExplorer

--- 08_biosamples_metadata/generate_census_of_assemblies.pl
+++ 08_biosamples_metadata/generate_census_of_assemblies.pl
@@ -10,7 +10,45 @@ warn "Getting NCBI genome assemblies from file '$assemblies_from_ncbi'\n";
 warn "Getting PhytoBacExplorer assemblies from file '$assemblies_from_phytobacexplorer'\n";
 
 my %biosample_has_assembly;
+my %biosample2phytobacexplorer;
 my %biosample2ncbi;
+
+### Open the phytobacexplorer assemblies file for reading
+open my $in_phytobacexplorer, '<:encoding(UTF-8)', $assemblies_from_phytobacexplorer
+    or die "Cannot open $assemblies_from_phytobacexplorer: $!";
+
+### Get the header line
+my $header_line = <$in_phytobacexplorer>;
+chomp $header_line;
+my @phytobacexplorer_headings = split /\t/, $header_line;
+
+### List the phytobacexplorer metadata headings
+warn "Headings from phytobacexplorer assemblies file:\n";
+foreach my $heading (@phytobacexplorer_headings) {
+    warn "\t$heading\n";
+}
+
+### Read the phytobacexplorer assemblies
+while (my $readline = <$in_phytobacexplorer>) {
+    my %heading2datum;
+    chomp $readline;
+    my @data = split /\t/, $readline;
+    foreach my $heading (@phytobacexplorer_headings) {
+	my $datum = shift @data;
+	$heading2datum{$heading} = $datum;
+    }
+    my $biosample = $heading2datum{'Sample'};
+    my $status = $heading2datum{'Status'};
+    my $assembly_accessions = $heading2datum{'Uberstrain'};
+    if ($status =~m/assembled/i) {
+	if (defined $biosample) {
+	    $biosample2phytobacexplorer{$biosample}{'assembly_accessions'} .= $assembly_accessions; 
+	    $biosample_has_assembly{$biosample} ++;
+	}   
+    }
+}
+### Close the phytobacexplorer assemblies file for reading 
+close $in_phytobacexplorer;
 
 ### Open the NCBI assemblies file for reading
 open my $in_ncbi, '<:encoding(UTF-8)', $assemblies_from_ncbi
@@ -27,26 +65,26 @@ foreach my $heading (@ncbi_headings) {
     warn "\t$heading\n";
 }
 
-### Read the NCBI assemblies
+### Read the NCBI assemblies                                                                                                                                            
 while (my $readline = <$in_ncbi>) {
     my %heading2datum;
     chomp $readline;
     my @data = split /\t/, $readline;
     foreach my $heading (@ncbi_headings) {
-	my $datum = shift @data;
-	$heading2datum{$heading} = $datum;
+        my $datum = shift @data;
+        $heading2datum{$heading} = $datum;
     }
     my $biosample = $heading2datum{'biosample'};
     my $has_assembly = $heading2datum{'has_assembly'};
     my $assembly_accessions = $heading2datum{'assembly_accessions'};
     if ($has_assembly =~m/yes/i) {
-	if (defined $biosample) {
-	    $biosample2ncbi{$biosample}{'assembly_accessions'} .= $assembly_accessions; 
-	    $biosample_has_assembly{$biosample} ++;
-	}   
+        if (defined $biosample) {
+            $biosample2ncbi{$biosample}{'assembly_accessions'} .= $assembly_accessions;
+            $biosample_has_assembly{$biosample} ++;
+        }
     }
 }
-### Close the NCBI assemblies file for reading 
+### Close the NCBI assemblies file for reading
 close $in_ncbi;
 
 ### How many BioSamples have at least one assembly?
@@ -55,10 +93,23 @@ warn "$count BioSamples have at least one assembly\n";
 
 ### Print a summary of the assemblies
 foreach my $biosample( sort keys %biosample_has_assembly) {
-    #warn "Checking BioSample $biosample\n";
     my $ncbi_accessions = $biosample2ncbi{$biosample}{'assembly_accessions'};
+    my $phytobacexplorer_accessions = $biosample2phytobacexplorer{$biosample}{'assembly_accessions'};
+
+    ### Resolve any undefined values
     if (defined $ncbi_accessions) {
-	warn "$biosample\t$ncbi_accessions\n";
+	# OK
+    } else {
+	$ncbi_accessions = '';
     }
+    if (defined $phytobacexplorer_accessions) {
+	# OK
+    } else {
+	$phytobacexplorer_accessions = '';
+    }
+
+    ### Print the info
+    warn "$biosample\t$ncbi_accessions\t$phytobacexplorer_accessions\n";
+
 }
 
